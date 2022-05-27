16c8d427ba5954973e8a3cc3772f923e2648f2ce
Billy <billydevbusiness@gmail.com>
2022-05-27 08:54:46 +0300

    statusbar

--- muffin/components/statusbar/__init__.py
+++ muffin/components/statusbar/__init__.py
@@ -1,8 +1,26 @@
 import tkinter as tk
+from tkinter import ttk
+
+from .item import StatusbarItem
 
 
 class Statusbar(tk.Frame):
     def __init__(self, master, *args, **kwargs):
         super().__init__(master, *args, **kwargs)
         self.master = master
-        self.config(height=1)
+        self.config(bd=1, bg='white', relief=tk.SUNKEN)
+
+        for i in ("Python", "UTF-8", "Ln y, Col x"):
+            self.add_item(i, tk.RIGHT)
+
+        for i in ("branch",):
+            self.add_item(i, tk.LEFT)
+
+    def add_item(self, name, side):
+        item = StatusbarItem(self, name=name)
+        item.pack(fill=tk.Y, side=side, padx=2)
+        self.add_separator(side)
+
+    def add_separator(self, side):
+        sep = ttk.Separator(self, orient=tk.VERTICAL)
+        sep.pack(side=side, fill=tk.Y, pady=1)
--- muffin/components/statusbar/item.py
+++ muffin/components/statusbar/item.py
@@ -2,6 +2,10 @@ import tkinter as tk
 
 
 class StatusbarItem(tk.Frame):
-    def __init__(self, master, *args, **kwargs):
+    def __init__(self, master, name, *args, **kwargs):
         super().__init__(master, *args, **kwargs)
         self.master = master
+        self.name = name
+
+        self.label = tk.Label(self, text=name, bg='white', padx=2)
+        self.label.pack(fill=tk.BOTH)
